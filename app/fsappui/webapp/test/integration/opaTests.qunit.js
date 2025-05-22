118472b6d6a3d6614f718e2c48095d1e7a485a92
Syam karukola <syamkarukola@gmail.com>
2025-05-22 12:18:59 +0000

    first commit

---
+++ app/fsappui/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'fsappui/test/integration/FirstJourney',
+		'fsappui/test/integration/pages/BooksList',
+		'fsappui/test/integration/pages/BooksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, BooksList, BooksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('fsappui') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheBooksList: BooksList,
+					onTheBooksObjectPage: BooksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
